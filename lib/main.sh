04abc9e9bf6f80e1ead2b3c9a0f42d188c97d8cb
Michael Born <mborn319@gmail.com>
2018-05-18 22:30:11 -0400

    Hmm, having issues with that curl auth header

--- lib/main.sh
+++ lib/main.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-## Easy variable for auth header
-mlite_auth_header="X-MailerLite-ApiKey: $mlite_api_key";
-
-echo "Connecting to Mailerlite API:" $mlite_api_uri;
+# functions and utilities
