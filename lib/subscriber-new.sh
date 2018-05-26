cfe29fc57d04030579ac1a2d20e683a4d5bba726
Michael Born <mborn319@gmail.com>
2018-05-26 11:34:33 -0400

    Echo the curl URL for ./mlite-subscriber --new.
    Easier debugging.

--- lib/subscriber-new.sh
+++ lib/subscriber-new.sh
@@ -5,7 +5,8 @@ echo "Creating new subscriber!";
 # Setup JSON body for request
 d="{\"name\":\"$arg_name\", \"email\":\"$arg_email\"}";
 
-curl $url_subscriber \
+echo "curl -X POST $url_subscriber --data \'$d\'";
+curl -X POST $url_subscriber \
   --data "$d" \
   -H "Content-Type: application/json" \
   -H "X-MailerLite-ApiKey:$mlite_api_key";
