b1aa5f9fe50811133c46dd3a4f4debdc2f3ec63e
Michael Born <mborn319@gmail.com>
2018-05-26 11:55:10 -0400

    Move group conditional to subscriber-get.sh.
    I don't know, I just like the ./mlite-subscriber file to hold argument
    stuff only.

--- lib/subscriber-get.sh
+++ lib/subscriber-get.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Getting all subscribers";
+if [[ "$arg_group" ]]; then
+  echo "Getting subscribers for group: $arg_group";
+  export url_subscriber="${mlite_api_uri}groups/$arg_group/subscribers";
+else
+  echo "Getting all subscribers";
+fi
 
 echo "curl $url_subscriber";
 curl $url_subscriber \
